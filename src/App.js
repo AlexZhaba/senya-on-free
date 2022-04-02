bf06a6aebe10e3bb448a44a699215a8882360b7b
Alexandr Zhavoronkov <sasazhava13@gmail.com>
2022-04-02 04:16:56 +0300

    initial senya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Senya from "./senya.jpeg";
 
-function App() {
+const initialTime = new Date("March 29, 2022 00:00:00");
+
+const App = () => {
+  const [timer, setTimer] = useState(Date.now() - initialTime.getTime());
+  const [currentTime, setCurrentTime] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer => timer + 1000);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    setCurrentTime(new Date(timer))
+  }, [timer]);
+
+  if (!currentTime) return <div>Loading</div>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <span>Senya is free</span>
       </header>
+      <main className="main">
+        <div>{ currentTime.getUTCFullYear() - 1970 } year</div>
+        <div>{ currentTime.getUTCMonth() } month</div>
+        <div>{ currentTime.getUTCDay() } days</div>
+        <div>{ currentTime.getUTCHours() } hours</div>
+        <div>{ currentTime.getUTCMinutes() } minutes</div>
+        <div>{ currentTime.getUTCSeconds() } seconds</div>
+      </main>
     </div>
-  );
-}
+  )
+};
 
 export default App;
